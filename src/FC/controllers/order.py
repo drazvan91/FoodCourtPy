76c148c73ebb400fb953f825138a2da966a4617a
Razvan <razvan@ubuntu.(none)>
2013-01-26 20:02:39 +0200

    base mobile template

--- src/FC/controllers/order.py
+++ src/FC/controllers/order.py
@@ -1,6 +1,29 @@
 from FC.models import Restaurant
 from django.shortcuts import render_to_response
+from FC.models.ProductCategory import ProductCategory
+from datetime import date,timedelta
 
 def order(request):
     restaurants = Restaurant.objects.all()
     return render_to_response("order/order.html", {"restaurants":restaurants})
+
+def m_order_create_step1(request):
+    restaurants=Restaurant.objects.all()
+    return render_to_response("m_order/orderStep1.html",{"restaurants":restaurants})
+
+def m_order_create_step2(request,restaurantId):
+    restaurant=Restaurant.objects.get(id=restaurantId);
+    days=[]
+    tday=date.today()
+    for i in range(1,8):
+        days.append(tday+timedelta(days=i))
+    return render_to_response("m_order/orderStep2.html",
+                              {"days":days,
+                               "restaurant":restaurant})
+
+def m_order_create_step3(request,restaurantId,year,month,day):
+    restaurant=Restaurant.objects.get(id=restaurantId);
+    categories=ProductCategory.objects.filter(restaurant=restaurant);
+    return render_to_response("m_order/orderStep3.html",
+                              {"categories":categories,
+                               "restaurant":restaurant})
--- src/FoodCourt/urls.py
+++ src/FoodCourt/urls.py
@@ -17,5 +17,13 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^account/', include('FC.urls')),
     url(r'^order/', 'FC.controllers.order.order'),
+    
+    url(r'^m/order/$',
+        'FC.controllers.order.m_order_create_step1'),
+    url(r'^m/order/(?P<restaurantId>\d+)/$',
+        'FC.controllers.order.m_order_create_step2'),
+    url(r'^m/order/(?P<restaurantId>\d+)/(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})/$',
+        "FC.controllers.order.m_order_create_step3"),
+    
     url(r'^admin/', include(admin.site.urls)),
 )
